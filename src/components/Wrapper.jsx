61e37a70f3236d9f2ccc09400998b4a52033de7a
Ode42 <ode.progr@outlook.com>
2022-02-13 20:57:52 +0200

    Add habit component

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -3,10 +3,18 @@ import ShowHabits from "./habits/ShowHabits";
 import { useState } from "react";
 
 const Wrapper = () => {
+  const [habits, setHabits] = useState([]);
+
+  const addHabit = (habit) => {
+    const newHabits = habits;
+    newHabits.push(habit);
+    setHabits(newHabits);
+  };
+
   return (
     <div>
-      <AddHabit />
-      <ShowHabits />
+      <AddHabit habits={habits} />
+      <ShowHabits callback={addHabit} />
     </div>
   );
 };
--- src/components/habits/AddHabit.jsx
+++ src/components/habits/AddHabit.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 const AddHabit = () => {
   return (
     <div className="addHabit">
       <h1>Add habit</h1>
+      <form>
+        <label for="name">Habit name: </label>
+        <input name="name" />
+        <button type="button">Send habit</button>
+      </form>
     </div>
   );
 };
